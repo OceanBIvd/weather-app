cdedd1e1a92b2d9a6751566f9f1772028860b216
Diego Yanez <diegooyanez195@gmail.com>
2023-05-01 18:05:53 -0700

    Added dummy data

--- script.js
+++ script.js
@@ -21,3 +21,14 @@ function success(position){
 	longitude = position.coords.longitude;
     console.log(latitude, longitude);
 }
+
+const btn = document.getElementById('getWeatherBtn');
+btn.addEventListener("click",function(){
+    let forecast = [["M", 52], ["Tu", 53], ["W", 54], ["Th", 55], ["F", 56]]
+    let forecastElements = document.getElementsByClassName("forecast");
+    for (let i = 0; i < forecast.length; i++) {
+        forecastElements[i].innerHTML = forecast[i][0] + ": " + forecast[i][1] + "\u00B0F";
+      }
+
+})
+

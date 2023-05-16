9f02ad8a122da863bcf17416837f1f482808b5b0
Diego Yanez <diegooyanez195@gmail.com>
2023-05-15 17:52:07 -0700

    Update

--- script.js
+++ script.js
@@ -7,13 +7,13 @@ window.onload = function(){
     const date = new Date();
     const dateString = (date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear();
     document.getElementById('date').innerHTML = dateString;
-}
 
 if ("geolocation" in navigator) {
     navigator.geolocation.getCurrentPosition(success)
 
 } else {
   console.log("Geolocation is not available in your browser.");
+}
 }
 
 function success(position){
@@ -23,12 +23,33 @@ function success(position){
 }
 
 const btn = document.getElementById('getWeatherBtn');
-btn.addEventListener("click",function(){
-    let forecast = [["M", 52], ["Tu", 53], ["W", 54], ["Th", 55], ["F", 56]]
+btn.addEventListener("click",function(){    
+    const xhr = new XMLHttpRequest();
+
+    xhr.open("GET", `http://localhost:3000/weather/${latitude}/${longitude}`);
+    xhr.send();
+
+    xhr.onload = function() {
+	    const body = JSON.parse(xhr.responseText)
+        let temperature = body.temperature
+	    let weatherStatus = body.weatherStatus
+        document.getElementById("temperature").innerHTML = `Temperature: ${temperature}\u00B0F`
+        document.getElementById("weatherStatus").innerHTML = `Weather Status: ${weatherStatus}`
+}
+
+//-------------------------------Forecast-----------------------------------------
+const xhr2 = new XMLHttpRequest();
+xhr2.open("GET", `http://localhost:3000/weather/${latitude}/${longitude}`);
+xhr2.send();
+
+xhr2.onload = function() {
+	const body = JSON.parse(xhr2.responseText)
+	let forecast = body;
     let forecastElements = document.getElementsByClassName("forecast");
-    for (let i = 0; i < forecast.length; i++) {
-        forecastElements[i].innerHTML = forecast[i][0] + ": " + forecast[i][1] + "\u00B0F";
-      }
+	for (let i = 0; i < forecast.length; i++) {
+		forecastElements[i].innerHTML = forecast[i].dayName + ": " + forecast[i].temp + "\u00B0F";
+        }
+    } 
 
 })
 
